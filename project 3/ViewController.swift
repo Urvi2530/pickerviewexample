3d0a4e376b9e8a00931c618a284b96f79adca9c6
COE-05 <coe-05@coe-05s-imac.local>
2019-08-29 15:48:47 +0530

    changed

--- project 3/ViewController.swift
+++ project 3/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
+    
+    @IBOutlet weak var pickerview: UIPickerView!
+    var arrTech = [".Net","Java","PHP"]
+    
+    //data source is started
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return arrTech.count
+    }
+    //data source is end
+    
+    //delegate methods need to create
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arrTech[row]
+    }
+    
+    //end delegate
+    
+    //button code for add data array and display in picker view
+    
+    @IBOutlet weak var txttech: UITextField!
+    
+   
+    
+    @IBAction func btnsavedataarray(_ sender: Any) {
+        arrTech.append(txttech.text!)
+        pickerview.reloadAllComponents()
+    }
+    
+        // Do any additional setup after loading the view, typically from a nib.
+   
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
 
 
+
 }
+
+
+
+
+
 
